8149b609397eb0dce419a5f4fc5bfbe438f97c6c
tjwls11 <09sj03@naver.com>
2025-06-17 09:04:17 +0900

    모듈수정정

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const db = require('./db')
 const session = require('express-session')
 const dotenv = require('dotenv')
